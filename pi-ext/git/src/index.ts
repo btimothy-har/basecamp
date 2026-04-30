a6ceeb4234e0e9463747168d72e9126564f720b9
Brian Har <62061780+btimothy-har@users.noreply.github.com>
2026-04-30 18:03:43 +0800

    Wire issue publish tool

--- pi-ext/git/src/index.ts
+++ pi-ext/git/src/index.ts
@@ -5,6 +5,7 @@
 import type { ExtensionAPI } from "@mariozechner/pi-coding-agent";
 import { registerCommands } from "./commands";
 import { registerGuards } from "./guards";
+import { registerIssueTool } from "./issue-tool";
 import { registerStatusTool } from "./status";
 import { registerTool } from "./tool";
 
@@ -13,4 +14,5 @@ export default function (pi: ExtensionAPI) {
 	registerCommands(pi);
 	registerStatusTool(pi);
 	registerTool(pi);
+	registerIssueTool(pi);
 }
