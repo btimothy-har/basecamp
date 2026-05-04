3029c5a2d9dd5f947413c7902ce1559befac2fca
Brian Har <62061780+btimothy-har@users.noreply.github.com>
2026-05-04 12:43:40 +0800

    refactor: compose pi extension from single entry

---
+++ pi-extension/src/index.ts
@@ -0,0 +1,20 @@
+import type { ExtensionAPI } from "@mariozechner/pi-coding-agent";
+import registerCapabilities from "../capabilities/src/index.ts";
+import registerEngineering from "../engineering/src/index.ts";
+import registerGit from "../git/src/index.ts";
+import registerModelAliases from "../model-aliases/src/index.ts";
+import registerProjects from "../projects/src/index.ts";
+import registerSession from "../session/src/index.ts";
+import registerWorkflow from "../workflow/src/index.ts";
+import registerWorkspace from "../workspace/src/index.ts";
+
+export default function (pi: ExtensionAPI): void {
+	registerWorkspace(pi);
+	registerModelAliases(pi);
+	registerSession(pi);
+	registerCapabilities(pi);
+	registerEngineering(pi);
+	registerProjects(pi);
+	registerWorkflow(pi);
+	registerGit(pi);
+}
