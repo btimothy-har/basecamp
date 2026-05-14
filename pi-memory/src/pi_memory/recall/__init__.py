8f833d12a3e0edafa337a14c80769c0378c2b96b
Brian Har <62061780+btimothy-har@users.noreply.github.com>
2026-05-14 22:33:11 +0800

    feat: add pi-memory raw transcript recall search

--- pi-memory/src/pi_memory/recall/__init__.py
+++ pi-memory/src/pi_memory/recall/__init__.py
@@ -1,5 +1,13 @@
-"""Recall indexing helpers for pi-memory."""
+"""Recall helpers for pi-memory."""
 
 from pi_memory.recall.indexing import TranscriptIndexResult, extract_search_text, index_transcript
+from pi_memory.recall.search import RawTranscriptRecallResult, RawTranscriptSearchResult, RecallSearchService
 
-__all__ = ["TranscriptIndexResult", "extract_search_text", "index_transcript"]
+__all__ = [
+    "RawTranscriptRecallResult",
+    "RawTranscriptSearchResult",
+    "RecallSearchService",
+    "TranscriptIndexResult",
+    "extract_search_text",
+    "index_transcript",
+]
